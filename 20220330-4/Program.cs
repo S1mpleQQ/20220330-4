3e3eaafc2c9ae2728ecad7566a6a471b3bbf26e1
user <user@A401-PC46>
2022-03-30 17:10:10 +0800

    加入專案檔案。

---
+++ 20220330-4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20220330_4
+{
+    class Program
+    {
+        private static int fac(int n)
+        {
+            if (n == 1)
+                return 1;
+            else
+                return n * fac(n-1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("20!={0}", fac(20));
+            Console.ReadLine();
+        }
+    }
+}
